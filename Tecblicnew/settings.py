ccbaf8fc86f9c816ddd2094e85b71d75018d52de
Vibhu0412 <vbpurohit1948@gmail.com>
2023-03-10 20:09:34 +0530

    dev change

--- Tecblicnew/settings.py
+++ Tecblicnew/settings.py
@@ -185,10 +185,7 @@ EMAIL_HOST_USER='sales@tecblic.com'
 EMAIL_HOST_PASSWORD='C@ntbre@k@#7459'
 
 
-# Custom Message Showing LAST
 # Custom Message Showing DEV
-# Custom Message Showing DEV1
-# Custom Message Showing DEV2
 MESSAGE_TAGS = {
         messages.DEBUG: 'alert-secondary',
         messages.INFO: 'alert-info',
